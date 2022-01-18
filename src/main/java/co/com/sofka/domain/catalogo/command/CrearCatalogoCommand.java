37f898a58cd035d15cced3cdd0ba6559b159842f
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2022-01-18 15:44:26 -0500

    Secrea command crearCatalogo

--- src/main/java/co/com/sofka/domain/catalogo/command/CrearCatalogoCommand.java
+++ src/main/java/co/com/sofka/domain/catalogo/command/CrearCatalogoCommand.java
@@ -1,2 +1,27 @@
-package co.com.sofka.domain.catalogo.command;public class CrearCatalogoCommand {
+package co.com.sofka.domain.catalogo.command;
+
+public class CrearCatalogoCommand {
+
+    private String catalogoTd;
+    private String nombre;
+
+    public CrearCatalogoCommand() {
+    }
+
+    public String getCatalogoTd() {
+        return catalogoTd;
+    }
+
+    public void setCatalogoTd(String catalogoTd) {
+        this.catalogoTd = catalogoTd;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
+
